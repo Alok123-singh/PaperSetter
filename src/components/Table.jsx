4452e45c56897577b2e876dfe3cfe50c647d5fa0
alok123-singh <aloksinghbais02@gmail.com>
2023-11-19 18:19:41 +0530

    updated style of cells of table, and updated text of buttons

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -61,15 +61,17 @@ function Table() {
 
     });
     const [percentage,setPercentage] = useState(() => {
-        const storedPercentage = JSON.parse(localStorage.getItem('percentage'));
-        if(storedPercentage){
-            return storedPercentage;
-        }
-
         let array = [];
-        for(let j = 1; j <= defaultColumns; j++)
-            array.push("0.00 % occupied");
-
+        for(let j = 0; j < defaultColumns; j++){
+            let cnt = 0;
+            for(let i = 0; i < defaultRows; i++){
+                if(matrix[i][j] == 1) cnt++;
+            }
+            let per = ((cnt / defaultRows) * 100).toFixed(2);
+            let message = per.toString() + " %";
+            array.push(message);
+        }
+            
         return array;
     });
 
@@ -184,14 +186,12 @@ function Table() {
 
 
     // Step 2: Save the matrix to local storage whenever it changes
-    useEffect(() => {
-    if(matrix.length === 0) return;
-        console.log("Data stored!");
-        localStorage.setItem('matrix', JSON.stringify(matrix));
-        localStorage.setItem('percentage', JSON.stringify(percentage));
-        console.log("Matrix ",matrix);
-        console.log("Percentage",percentage);
-    }, [matrix]);
+    // useEffect(() => {
+    // if(matrix.length === 0) return;
+    //     console.log("Matrix stored in local storage !");
+    //     localStorage.setItem('matrix', JSON.stringify(matrix));
+    //     // console.log("Matrix ",matrix);
+    // }, [matrix]);
 
     // Step 3: Handle click on a block to mark/unmark it
     const handleBlockClick = (row, col) => {
@@ -260,7 +260,7 @@ function Table() {
                 if(matrix[i][index] === 1) cnt++;
             }
             const num = (cnt / defaultRows) * 100;
-            const result = num.toString() + " % occupied";
+            const result = num.toString() + " %";
             return result;
         });
 
@@ -269,13 +269,13 @@ function Table() {
     }, [matrix]);
 
     const showDay = (day) => {
-        if(day === 1) return ("Monday");
-        else if(day === 2) return "Tuesday";
-        else if(day === 3) return "Wednesday";
-        else if(day === 4) return "Thursday";
-        else if(day === 5) return "Friday";
-        else if(day === 6) return "Saturday";
-        return "Sunday";
+        if(day === 1) return ("Mon");
+        else if(day === 2) return "Tue";
+        else if(day === 3) return "Wed";
+        else if(day === 4) return "Thur";
+        else if(day === 5) return "Fri";
+        else if(day === 6) return "Sat";
+        return "Sun";
     };
 
     const formatTime = (timeInSeconds) => {
@@ -310,40 +310,40 @@ function Table() {
             })
         ))
         result = (cnt / (defaultRows * defaultColumns)) * 100;
-        return (result.toFixed(2).toString() + " %");
+        return ("Result - " + result.toFixed(2).toString() + " %");
     };
     
     return (
         <div className='w-full h-auto flex flex-wrap justify-around items-center p-5'>
             <div className='w-full h-auto flex flex-col justify-center items-center '>
-                <div className='w-full flex items-center justify-around pl-[1.5rem]'>
+                <div className='w-full flex items-center justify-around'>
                     <textarea disabled={true} className=' w-[74%] h-[3rem] outline outline-black pt-3 rounded-3xl text-center queryShow'></textarea>
                     <div>
                         <button type='button' className={`${index > queries.length ? 'invisible' : 
-                        ''} yes w-[4rem] bg-green-600 hover:bg-green-500`}
+                        ''} yes w-[5rem] bg-green-600 hover:bg-green-500`}
                         onClick={() => setAllowed(true)}
                         >
-                            Yes
+                            Accept
                         </button>
 
-                        <button type='button' className={`${allowed || index > queries.length ? 'invisible' : ''} no ml-5 w-[4rem] bg-red-600 hover:bg-red-500`}
+                        <button type='button' className={`${allowed || index > queries.length ? 'invisible' : ''} no ml-5 w-[5rem] bg-red-600 hover:bg-red-500`}
                         onClick={() => setChange(true)}
                         >
-                            No
+                            Deny
                         </button>
                     </div>
                 </div>
 
                 <div className='w-full flex justify-end items-center pr-10'>
-                    <div className={`${index > queries.length ? 'invisible' : ''} pt-2`}>
+                    <div className={`${index > queries.length ? 'invisible' : ''} pt-4`}>
                         <p>Time Remaining : {formatTime(counter)}</p>
                     </div>
                 </div>
             </div>
 
-            <div className=' cursor-default'>
+            <div className=' w-[80%] cursor-default'>
                 <div className='flex '>
-                    <div className='w-[8rem] border border-solid border-gray-300 text-left p-2 bg-blue-400 rounded-md'>
+                    <div className='w-[8rem] p-2 border border-solid border-gray-300 flex justify-center items-center bg-blue-400 rounded-md'>
                         Day
                     </div>
                     {matrix.map((array,index1) => (
@@ -358,17 +358,17 @@ function Table() {
                 {matrix.map((row, rowIndex) => (
 
                     <div className='flex' key={rowIndex}>
-                        <div className='w-[8rem] h-auto border border-solid border-gray-300  p-2 bg-emerald-400 rounded-md'>
+                        <div className='w-[8rem] p-2 flex justify-center items-center border border-solid border-gray-300 bg-emerald-400 rounded-md'>
                             {"Room " + (rowIndex+1).toString()}
                         </div>
                         {row.map((value, colIndex) => (
                             <div
 
                             key={colIndex}
-                            className={`${ allowed && selectableColumn[colIndex] && (rooms > 0 || selectableRow[rowIndex] === true) ? '' : 'pointer-events-none'} w-[8rem] border border-solid border-gray-300 text-left p-2 rounded-md  ${value === 1 ? 'bg-red-500 hover:bg-rose-600' : 'bg-white hover:bg-gray-300'} text-gray-200`}
+                            className={`${ allowed && selectableColumn[colIndex] && (rooms > 0 || selectableRow[rowIndex] === true) ? '' : 'pointer-events-none'} w-[8rem] p-2 flex justify-center items-center border border-solid border-gray-300 text-left rounded-md  ${value === 1 ? 'bg-red-500 hover:bg-rose-600' : 'bg-white hover:bg-gray-300'} text-gray-200`}
                             onClick={() => handleBlockClick(rowIndex, colIndex)}
                             >
-                                {}
+                                {"click to book"}
                             </div>
                             
                         ))}
@@ -376,9 +376,9 @@ function Table() {
                 ))}
 
                 <div className='flex'>
-                    <div className='w-[8rem] border border-solid border-gray-300 text-left p-2 bg-red-500 rounded-md'>Total</div>
+                    <div className='w-[8rem] p-2 border border-solid border-gray-300 flex justify-center items-center bg-red-500 rounded-md'>Total</div>
                     {percentage.map((value,index) => (
-                        <div key={index} className='w-[8rem] border border-solid border-gray-300 text-left p-2 bg-red-400 rounded-md'>
+                        <div key={index} className='w-[8rem] flex justify-center items-center border border-solid border-gray-300 p-2 bg-red-400 rounded-md'>
                             {value};
                         </div>
                     ))}
@@ -394,7 +394,7 @@ function Table() {
                     {index < queries.length ? 'Next Query' : 'End Game'}
                 </button>
 
-                <div className={`${index <= queries.length ? 'invisible': ''} w-[6rem] h-[2.2rem] p-2 bg-blue-400 flex justify-center items-center rounded-lg `}>
+                <div className={`${index <= queries.length ? 'invisible': ''} w-[10rem] h-[2.2rem] p-2 bg-blue-400 flex justify-center items-center rounded-lg `}>
                     {showResult()}
                 </div>
 
